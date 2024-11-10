f9e54136d8a857149e19cec9bf9fe22ea57222a3
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-11-10 16:51:31 +0700

    a

--- projects/section2/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
+++ projects/section2/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
@@ -12,10 +12,17 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((req) -> req.anyRequest().authenticated());
+        // http.authorizeHttpRequests((req) -> req.anyRequest().authenticated());
+        // http.authorizeHttpRequests((req) -> req.anyRequest().permitAll());
+
+        http.authorizeHttpRequests((req) -> req.requestMatchers("/api/accounts", "/api/balances", "/api/loans", "/api/cards").authenticated());
+        http.authorizeHttpRequests((req) -> req.requestMatchers("/api/notices", "/api/contacts", "/error").permitAll());
 
         http.formLogin(withDefaults());
+        // http.formLogin((httpSecurityFormLoginConfig) -> httpSecurityFormLoginConfig.disable()); // chamge withDefaults() to disable() for disable login form
+        
         http.httpBasic(withDefaults());
+        // http.httpBasic((httpBasicConfig) -> httpBasicConfig.disable());
 
         return http.build();
     }
--- projects/section2/src/main/java/com/lecsures/section2/controllers/LoanController.java
+++ projects/section2/src/main/java/com/lecsures/section2/controllers/LoanController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/loons")
-public class LoonController {
+@RequestMapping("/api/loans")
+public class LoanController {
 
     @GetMapping("")
     public ResponseEntity<String> getLoonDetails() {
