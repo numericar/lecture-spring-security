532170d2a9bd720a04db4ae45973bf31145a4763
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-11-11 21:11:52 +0700

    user using in memory

--- projects/section2/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
+++ projects/section2/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
@@ -3,6 +3,10 @@ package com.lecsures.section2.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,6 +29,14 @@ public class SecurityConfig {
         // http.httpBasic((httpBasicConfig) -> httpBasicConfig.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("user").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
