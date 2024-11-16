3754f80826ecda913ccb304b4baf19d7f9f42578
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-11-16 13:59:22 +0700

    add customer with repository

--- projects/section3/src/main/java/com/lecsures/section2/EazyBankBackedApplication.java
+++ projects/section3/src/main/java/com/lecsures/section2/EazyBankBackedApplication.java
@@ -2,8 +2,10 @@ package com.lecsures.section2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class EazyBankBackedApplication {
 
 	public static void main(String[] args) {
