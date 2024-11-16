8d6cb9de9a5a037bb13f95e12e5b06578a17e90e
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-11-16 14:34:22 +0700

    EazyBankUserDetailsService

--- projects/section3/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
+++ projects/section3/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
@@ -4,16 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -43,10 +39,10 @@ public class SecurityConfig {
     //     return new InMemoryUserDetailsManager(user, admin);
     // }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
+    // @Bean
+    // public UserDetailsService userDetailsService(DataSource dataSource) {
+    //     return new JdbcUserDetailsManager(dataSource);
+    // }
 
     @Bean 
     public PasswordEncoder passwordEncoder() {
