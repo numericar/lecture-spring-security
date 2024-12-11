d453cf40477fdcd2e9c6d7b17c51561a5c9b791b
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-11 20:23:52 +0700

    Update SecurityConfig.java

--- projects/section7/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
+++ projects/section7/src/main/java/com/lecsures/section2/configs/SecurityConfig.java
@@ -3,15 +3,12 @@ package com.lecsures.section2.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import com.lecsures.section2.exceptions.CustomBasicAuthenticationEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
