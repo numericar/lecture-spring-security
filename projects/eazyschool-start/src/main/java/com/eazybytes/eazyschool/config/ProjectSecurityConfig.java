78578a292b35f891ed675ba11ab0a8653735f9e3
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-12 12:24:33 +0700

    mvc custom login page

---
+++ projects/eazyschool-start/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> 
+                    requests.requestMatchers("/dashboard").permitAll()
+                            .requestMatchers("/", "/home", "/holidays/**", "/contact", "/saveMsg", "/courses", "/about", "/assets/**", "/login/**").permitAll())
+                .formLogin(flc -> 
+                    flc.loginPage("/login") // invoke login page
+                       .usernameParameter("usern") // custom field username from HMTL
+                       .passwordParameter("pwd") // custom field password from HTML
+                       .defaultSuccessUrl("/dashboard")  // set default success url after login successful
+                    ) 
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}EazyBytes@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m")
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * From Spring Security 6.3 version
+     *
+     * @return
+     */
+    @Bean
+    CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+
+}
